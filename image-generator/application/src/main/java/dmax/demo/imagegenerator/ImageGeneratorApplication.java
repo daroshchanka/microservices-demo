68a88dd126237dfc6d4d23d8860acd6d08478871
Max Daroshchanka <max.daroshchanka@symmetrical.ai>
2024-08-21 00:36:02 +0300

    image-generator: remove datasource/web-server dependencies

--- image-generator/application/src/main/java/dmax/demo/imagegenerator/ImageGeneratorApplication.java
+++ image-generator/application/src/main/java/dmax/demo/imagegenerator/ImageGeneratorApplication.java
@@ -2,15 +2,22 @@ package dmax.demo.imagegenerator;
 
 import dmax.demo.documents.feign.DocumentsClient;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @EnableFeignClients(clients = DocumentsClient.class)
 @SpringBootApplication
 public class ImageGeneratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ImageGeneratorApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication application = new SpringApplication(ImageGeneratorApplication.class);
+    application.setWebApplicationType(WebApplicationType.NONE);
+    application.run(args);
+  }
 
 }
