ff3d03af69e5aead7533731aa37bab3650f2fc41
Max Daroshchanka <max.daroshchanka@symmetrical.ai>
2024-08-26 20:31:46 +0300

    documents: add service layer

--- documents/application/src/main/java/dmax/demo/documents/controllers/DocumentsController.java
+++ documents/application/src/main/java/dmax/demo/documents/controllers/DocumentsController.java
@@ -1,9 +1,11 @@
 package dmax.demo.documents.controllers;
 
 import dmax.demo.documents.models.FileUploadResponseMessage;
+import dmax.demo.documents.services.FileStorageService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,50 +27,29 @@ import java.util.UUID;
 @RequestMapping("/api")
 public class DocumentsController {
 
-  private static final Path UPLOADS = Paths.get("uploads");
+  @Autowired
+  private FileStorageService fileStorageService;
 
   @GetMapping(
       value = "/files/{fileId}",
       produces = MediaType.IMAGE_PNG_VALUE
   )
   public @ResponseBody byte[] getFileById(@PathVariable("fileId") UUID fileId) throws IOException {
-    File initialFile = resolveFile(fileId);
-    if (!initialFile.exists()) {
+    if (!fileStorageService.isFileExists(fileId)) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND, "fileId " + fileId + " not found");
     }
-    try (InputStream targetStream = FileUtils.openInputStream(initialFile)) {
-      return IOUtils.toByteArray(targetStream);
-    }
+    return fileStorageService.getFileContent(fileId);
   }
 
   @PostMapping(value = "/files/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   public ResponseEntity<FileUploadResponseMessage> uploadFile(
       @RequestParam("file") MultipartFile file, @RequestPart("fileId") String fileId) {
-    String message = "";
     try {
-      if (!UPLOADS.toFile().exists()) {
-        Files.createDirectories(UPLOADS);
-      }
-      File initialFile = resolveFile(UUID.fromString(fileId));
-      try {
-        Files.copy(file.getInputStream(), initialFile.toPath());
-      } catch (Exception e) {
-        if (e instanceof FileAlreadyExistsException) {
-          throw new RuntimeException("A file of that name already exists.");
-        }
-        throw new RuntimeException(e.getMessage());
-      }
-      message = "Uploaded the file successfully: " + UPLOADS.toFile().getAbsolutePath() + "/" + file.getOriginalFilename();
-      System.out.println(message);
-      return ResponseEntity.status(HttpStatus.OK).body(new FileUploadResponseMessage(message));
+      String successMessage = fileStorageService.doUpload(file, UUID.fromString(fileId));
+      return ResponseEntity.status(HttpStatus.OK).body(new FileUploadResponseMessage(successMessage));
     } catch (Exception e) {
-      message = "Could not upload the file: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
-      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new FileUploadResponseMessage(message));
+      return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new FileUploadResponseMessage(e.getMessage()));
     }
-  }
-
-  private File resolveFile(UUID fileId) {
-    return new File(UPLOADS.toFile(), fileId + ".png");
   }
 
 }
---
+++ documents/application/src/main/java/dmax/demo/documents/services/FileStorageService.java
@@ -0,0 +1,53 @@
+package dmax.demo.documents.services;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class FileStorageService {
+
+  private static final Path UPLOADS = Paths.get("uploads");
+
+  public boolean isFileExists(@NotNull UUID fileId) {
+    return resolveFile(fileId).exists();
+  }
+
+  @SneakyThrows
+  public byte[] getFileContent(@NotNull UUID fileId) {
+    try (InputStream targetStream = FileUtils.openInputStream(resolveFile(fileId))) {
+      return IOUtils.toByteArray(targetStream);
+    }
+  }
+
+  private File resolveFile(@NotNull UUID fileId) {
+    return new File(UPLOADS.toFile(), fileId + ".png");
+  }
+
+  /**
+   * @return success message
+   * */
+  public String doUpload(@NotNull MultipartFile file, @NotNull UUID fileId) {
+    String message = "";
+    try {
+      if (!UPLOADS.toFile().exists()) {
+        Files.createDirectories(UPLOADS);
+      }
+      Files.copy(file.getInputStream(), resolveFile(fileId).toPath());
+      message = "Uploaded the file successfully: " + UPLOADS.toFile().getAbsolutePath() + "/" + file.getOriginalFilename();
+    } catch (Exception e) {
+      throw new RuntimeException("Could not upload the file: " + file.getOriginalFilename() + ". Error: " + e.getMessage(), e);
+    }
+    return message;
+  }
+}
